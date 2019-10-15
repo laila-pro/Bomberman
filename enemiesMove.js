3fc6f18496c04584b2e0dcad50fd4db3b3863ae5
blothe <b.lothe@codeur.online>
2019-10-15 09:30:28 +0200

    Add wall-block for enemies

--- enemiesMove.js
+++ enemiesMove.js
@@ -1,3 +1,11 @@
+/* Enregistre les coordonnées des murs pour évaluer la faisabilité des déplacements. */
+
+var wallObjects = document.querySelectorAll("div.wall"); // liste tous les objets "wall"
+var wallCoordinates = ["wallCoordinates"]; // liste toutes les coordonnées de ces objets
+for (var wallObject of wallObjects) {
+  wallCoordinates[wallObject.id.replace("wall", "")] = wallObject.style.left + "," + wallObject.style.top;
+}
+
 let enemies = document.getElementsByClassName("enemies");
 setInterval(enemiesMove, 1000);
 
@@ -8,28 +16,32 @@ function enemiesMove() {
     let direction = Math.floor(Math.random() * 4);
     switch (direction) {
       case 0:
-        if (enemy.offsetLeft < 768) {
-          enemy.style.left = enemy.offsetLeft + 32 + "px";
-          enemy.style.top = enemy.offsetTop + "px";
-        }
+      var positionTest = (enemy.offsetLeft + 32) + "px," + enemy.offsetTop + "px"; // calcul des coordonnées de test
+      if (enemy.offsetLeft < 768 && !wallCoordinates.includes(positionTest)) {
+        enemy.style.left = enemy.offsetLeft + 32 + "px";
+        enemy.style.top = enemy.offsetTop + "px";
+      }
       break;
       case 1:
-        if (enemy.offsetLeft > 0) {
-          enemy.style.left = enemy.offsetLeft - 32 + "px";
-          enemy.style.top = enemy.offsetTop + "px";
-        }
+      var positionTest = (enemy.offsetLeft - 32) + "px," + enemy.offsetTop + "px"; // calcul des coordonnées de test
+      if (enemy.offsetLeft > 0 && !wallCoordinates.includes(positionTest)) {
+        enemy.style.left = enemy.offsetLeft - 32 + "px";
+        enemy.style.top = enemy.offsetTop + "px";
+      }
       break;
       case 2:
-        if (enemy.offsetTop < 768) {
-          enemy.style.left = enemy.offsetLeft + "px";
-          enemy.style.top = enemy.offsetTop + 32 + "px";
-        }
+      var positionTest = enemy.offsetLeft + "px," + (enemy.offsetTop + 32) + "px"; // calcul des coordonnées de test
+      if (enemy.offsetTop < 768 && !wallCoordinates.includes(positionTest)) {
+        enemy.style.left = enemy.offsetLeft + "px";
+        enemy.style.top = enemy.offsetTop + 32 + "px";
+      }
       break;
       case 3:
-        if (enemy.offsetTop > 0) {
-          enemy.style.left = enemy.offsetLeft + "px";
-          enemy.style.top = enemy.offsetTop - 32 + "px";
-        }
+      var positionTest = enemy.offsetLeft + "px," + (enemy.offsetTop - 32) + "px"; // calcul des coordonnées de test
+      if (enemy.offsetTop > 0 && !wallCoordinates.includes(positionTest)) {
+        enemy.style.left = enemy.offsetLeft + "px";
+        enemy.style.top = enemy.offsetTop - 32 + "px";
+      }
       break;
       default: // do nothing
     }
